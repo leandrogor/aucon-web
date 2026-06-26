d0255a79733bb8af80574fe0e756c9d59a77c0e1
Leandro Gabriel Gor <leandrogabrielgor@gmail.com>
2026-06-26 15:19:57 -0300

    Mejora inicio

--- src/app/funcionalidades/inicio/pagina-inicio/pagina-inicio.component.ts
+++ src/app/funcionalidades/inicio/pagina-inicio/pagina-inicio.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed } from '@angular/core';
+import { Component, computed, signal, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { BotonWhatsappComponent } from '../../../compartido/componentes/boton-whatsapp/boton-whatsapp.component';
 
@@ -9,6 +9,23 @@ import { BotonWhatsappComponent } from '../../../compartido/componentes/boton-wh
   templateUrl: './pagina-inicio.component.html',
   styleUrl: './pagina-inicio.component.scss',
 })
-export class PaginaInicioComponent {
+export class PaginaInicioComponent implements AfterViewInit {
   readonly aniosTrayectoria = computed(() => new Date().getFullYear() - 1995);
+  readonly videoListo = signal(false);
+
+  @ViewChild('videoFondo') private readonly videoFondoRef!: ElementRef<HTMLVideoElement>;
+
+  ngAfterViewInit(): void {
+    // Cargamos el video en 2do plano, luego de que la página sea interactiva
+    const video = this.videoFondoRef.nativeElement;
+    video.src = 'assets/videos/tvbhd170.mp4';
+    video.load();
+    video.play().catch(() => {
+      /* autoplay puede estar bloqueado */
+    });
+  }
+
+  onVideoListo(): void {
+    this.videoListo.set(true);
+  }
 }
